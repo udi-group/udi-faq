622d6ba5b793e691ec9eac1d0646f5e7ad29579c
root <root@anonymous.localdomain>
2019-06-25 12:36:06 +0200

    Updates

---
+++ precache-manifest.d4debbbfad14b8c4ad23e5e0aca27d7a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "633a86d43945cbddceb776c88a8858bb",
+    "url": "/udi-faq/index.html"
+  },
+  {
+    "revision": "9a74d896d6cf110714f7",
+    "url": "/udi-faq/static/css/main.184f1582.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/udi-faq/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "9a74d896d6cf110714f7",
+    "url": "/udi-faq/static/js/main.41c0f1e7.chunk.js"
+  },
+  {
+    "revision": "8abe5bdcb55facafc9c2",
+    "url": "/udi-faq/static/js/runtime~main.71b012b8.js"
+  }
+]);
